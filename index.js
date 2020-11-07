918d3e229e3cbc0ba65c7414901d9d4f7efa8008
Damian Jacewicz <215739@edu.p.lodz.pl>
2020-11-07 15:03:41 +0100

    x

--- index.js
+++ index.js
@@ -66,13 +66,11 @@ const init = async () => {
         }
     });
 
-
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err);
     process.exit(1);
 });
