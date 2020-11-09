5c030fd1ce8dcc041580418329544c5284f01d58
pawellazicki <pawelllaz@gmail.com>
2020-11-10 00:25:40 +0100

    getting boards from db

--- frontend/src/containers/boards.container.jsx
+++ frontend/src/containers/boards.container.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Board from '../components/Board.component'
 import '../App.css';
+import {fetchBoards} from '../API/BoardsAPI';
+
 
 export default function Boards() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchBoards().then(result => {
+            setData(result);
+            setLoading(false);
+        })
+    }, []);
+
     return (
         <div className="tabs-container">
             <h2 className="myBoards">My Boards</h2>
             <div className="tabs">
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
-                <Board title="tablica" team="tango"></Board>
+                {data.map(board => (
+                    <Board key={board.id} title={board.title} team={board.team_name}></Board>
+                ))}
             </div>
         </div>
     )
